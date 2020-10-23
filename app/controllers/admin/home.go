a9fe0efbcf29209d1d4962297b506f0ba395aa86
Yanfan <330483691@qq.com>
2020-10-23 12:00:28 +0800

    chore: 首页统计

--- app/controllers/admin/home.go
+++ app/controllers/admin/home.go
@@ -3,6 +3,8 @@ package admin
 import (
 	"bbs/app/constants"
 	"bbs/app/funcs/response"
+	"bbs/app/model/posts"
+	"bbs/app/model/users"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
@@ -11,5 +13,12 @@ type HomeController struct{}
 
 // GET|后台首页
 func (c *HomeController) Home(r *ghttp.Request) {
-	response.ViewExit(r, constants.AdminLayoutTplPath, g.Map{"mainTpl": constants.AdminHomeTplPath})
+	userCount,_ := g.DB().Table(users.Table).Count()
+	postCount,_ := g.DB().Table(posts.Table).Count()
+
+	response.ViewExit(r, constants.AdminLayoutTplPath, g.Map{
+		"mainTpl": constants.AdminHomeTplPath,
+		"user": userCount,
+		"post": postCount,
+	})
 }
